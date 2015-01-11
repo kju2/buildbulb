79efaf2ad7986c37b22fa14c1b9811af69643d18
kju2 <kju2@edlinger.me>
2015-01-11 08:34:51 +0100

    Remove unnecessary LightProxy.

--- lib/buildbulb/light.rb
+++ lib/buildbulb/light.rb
@@ -23,55 +23,10 @@ module BuildBulb
             light = @lifx.lights.with_label(@label)
             if light.nil?
                 @logger.error("#{@label} wasn't found. Ignoring commands for #{@label}.")
-                LightProxy.new(@logger, @label, Ignore.new)
+                Ignore.new
             else
-                LightProxy.new(@logger, @label, light)
+                light
             end
-        end
-
-    end
-
-    class LightProxy
-
-        def initialize(logger, label, light)
-            @logger = logger
-            @label = label
-            @light = light
-        end
-
-        # Turns the light on synchronously.
-        # @return Light
-        def turn_on!
-            @logger.debug("#{@label}: turn on!")
-            @light.turn_on!
-            self
-        end
-
-        # Turns the light off synchronously.
-        # @return Light
-        def turn_off!
-            @logger.debug("#{@label}: turn off!")
-            @light.turn_off!
-            self
-        end
-
-        # @return [Boolean] Returns true if device is on.
-        def on?
-            @light.on?
-        end
-
-        # @return [Boolean] Returns true if device is off.
-        def off?
-            @light.off?
-        end
-
-        # Attempts to set the color of the light to `color` asynchronously.
-        # @param color [Color] The color to be set.
-        # @return Light
-        def set_color(color)
-            @logger.debug("#{@label} color is set to #{color}.")
-            @light.set_color(color)
-            self
         end
 
     end
